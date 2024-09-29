c6982b1a6be3d315321dfe19d5e961dce4628ac2
sazzadrupak <sazzad@oployeelabs.com>
2024-09-29 10:49:27 +0300

    Added not-found page (inspired from internet)

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,21 @@
+import { NavLink } from 'react-router-dom';
+
+import s from './NotFoundPage.module.scss';
+
+const NotFoundPage = () => {
+  return (
+    <div className={s.container}>
+      <div className={s.imageSection}>
+        <h1 className={s.fourZeroFourText}>404</h1>
+      </div>
+      <div className={s.content}>
+        <p className={s.message}>The page you are looking for not avaible!</p>
+        <NavLink to="/" className={s.button}>
+          Go to Home
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import { RouteObject } from 'react-router-dom';
 import App from './App';
 import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
 import UsersPage from './pages/users/UsersPage';
 
 const routes: RouteObject[] = [
   {
     path: '/',
     element: <App />,
+    errorElement: <NotFoundPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: '/users', element: <UsersPage /> },
